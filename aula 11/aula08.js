d2067c9a2119bc41ed50b4590ee77055731c49f0
Jefesson Santos <107877340+JefessonSantos@users.noreply.github.com>
2022-10-18 15:59:30 -0300

    Add files via upload

---
+++ aula 11/aula08.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){ //   Condição simples!
+    console.log(`Você ultrapassou a velociade permitida e está sendo MULTADO!`)
+}
+console.log(`Diriga sempre com cinto de segurança! `)
